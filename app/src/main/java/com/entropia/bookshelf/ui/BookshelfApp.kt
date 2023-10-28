e2258ccccf46de76d4d2dabe303c2fdf6c0ddc88
entropia <marta9kut@gmail.com>
2023-10-28 15:05:42 +0200

    started work on screens

--- app/src/main/java/com/entropia/bookshelf/ui/BookshelfApp.kt
+++ app/src/main/java/com/entropia/bookshelf/ui/BookshelfApp.kt
@@ -1,4 +1,28 @@
 package com.entropia.bookshelf.ui
 
-class BookshelfApp {
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.entropia.bookshelf.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BookshelfApp(modifier: Modifier) {
+    Scaffold(topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) }) }) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            // TODO
+        }
+    }
 }
